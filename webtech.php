15b8bf27c6330d33be8f20660e0ad337592f68c9
celiance <60871961+celiance@users.noreply.github.com>
2020-02-10 11:12:06 +0100

    Update webtech.php

--- webtech.php
+++ webtech.php
@@ -2,10 +2,11 @@
 <html lang="de" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Gaggi</title>
   </head>
   <body>
     <?php echo "yoooo"; ?>
     <h1>Hallo</h1>
+    <p>I like big bitts and I cannot lie</p>
   </body>
 </html>
